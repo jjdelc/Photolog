69392c1b6c756afefb58cc77ffbe960e78822ede
Jj <jjdelc@gmail.com>
2015-12-25 23:33:24 -0500

    Better file logging

--- photolog/tools/uploader.py
+++ photolog/tools/uploader.py
@@ -17,22 +17,26 @@ def read_local_conf(conf_file=None):
     return conf
 
 
-def upload_directory(directory, endpoint, secret, tags, skip):
-    total_files = 0
+def upload_directories(directories, endpoint, secret, tags, skip):
     start = time()
     first_batch, second_batch = [], []
-    for file in os.listdir(directory):
-        name, ext = os.path.splitext(file)
-        ext = ext.lstrip('.').lower()
-        if ext not in ALLOWED_FILES:
-            continue
-        full_file = os.path.join(directory, file)
-        if ext in IMAGE_FILES:
-            first_batch.append((file, full_file))
-        elif ext in RAW_FILES:
-            second_batch.append((file, full_file))
+    for directory in directories:
+        for file in os.listdir(directory):
+            name, ext = os.path.splitext(file)
+            ext = ext.lstrip('.').lower()
+            if ext not in ALLOWED_FILES:
+                continue
+            full_file = os.path.join(directory, file)
+            if ext in IMAGE_FILES:
+                first_batch.append((file, full_file))
+            elif ext in RAW_FILES:
+                second_batch.append((file, full_file))
 
+    total_files = len(first_batch) + len(second_batch)
+    n = 1
     for file, full_file in first_batch + second_batch:
+        log.info('Uploading %s [%s/%s]' % (full_file, n, total_files))
+        file_start = time()
         requests.post(endpoint, data={
             'tags': tags,
             'skip': skip,
@@ -42,8 +46,9 @@ def upload_directory(directory, endpoint, secret, tags, skip):
         }, headers={
             'X-PHOTOLOG-SECRET': secret
         })
-        log.info('Uploaded %s' % file)
-        total_files += 1
+        pct = 100 * n / total_files
+        log.info("Done in %0.2fs [%0.1f%%]" % (time() - file_start, pct))
+        n += 1
     elapsed = time() - start
     log.info('Uploaded %s files in %.2fs' % (total_files, elapsed))
 
@@ -53,7 +58,8 @@ def run():
     parser = argparse.ArgumentParser(
         description="Upload files or directories to Photolog"
     )
-    parser.add_argument('directory', type=str, help="Directory to upload")
+    parser.add_argument('directories', type=str, nargs='+',
+        help="Directory to upload")
     parser.add_argument('--tags', metavar='T', nargs='?', type=str,
         help="Tags for this batch")
     parser.add_argument('--host', metavar='H', nargs='?', type=str,
@@ -61,12 +67,13 @@ def run():
     parser.add_argument('--skip', nargs='?', type=str,
         help="steps to skip")
     parsed = parser.parse_args()
-    directory = os.path.realpath(parsed.directory)
+    import pdb;pdb.set_trace()
+    directories = [os.path.realpath(d) for d in parsed.directories]
     endpoint = urljoin(parsed.host or config['host'], '/photos/')
     secret = md5(config['secret'].encode('utf-8')).hexdigest()
     tags = parsed.tags or ''
     skip = parsed.skip or ''
-    upload_directory(directory, endpoint, secret, tags, skip)
+    upload_directories(directories, endpoint, secret, tags, skip)
 
 
 if __name__ == '__main__':
