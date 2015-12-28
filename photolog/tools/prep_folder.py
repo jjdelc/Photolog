833b2d4080760845cdfce10cb03952df0236c2eb
Jj <jjdelc@gmail.com>
2015-12-28 00:33:04 -0500

    Add prep folder

---
+++ photolog/tools/prep_folder.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+from photolog import ALLOWED_FILES, IMAGE_FILES, RAW_FILES
+
+BASE_COMMAND = 'upload2photolog'
+
+
+def run():
+    parser = argparse.ArgumentParser(
+        description="Prepare directory for upload"
+    )
+    parser.add_argument('directory', type=str,
+        help="Directory to upload")
+    parser.add_argument('--tags', metavar='T', nargs='?', type=str,
+        help="Tags for this batch")
+    parser.add_argument('--host', metavar='H', nargs='?', type=str,
+        help="Host to upload")
+    parser.add_argument('--skip', nargs='?', type=str,
+        help="steps to skip")
+    parser.add_argument('--output', type=str,
+        help="File to write commands")
+    parsed = parser.parse_args()
+
+    directory = parsed.directory
+    tags = parsed.tags or ''
+    skip = parsed.skip or ''
+    host = parsed.host or ''
+    output = parsed.output or ''
+    lines, first_batch, second_batch = [], [], []
+
+    for file in sorted(os.listdir(directory)):
+        name, ext = os.path.splitext(file)
+        ext = ext.lstrip('.').lower()
+        if ext not in ALLOWED_FILES:
+            continue
+        full_file = os.path.join(directory, file)
+        if ext in IMAGE_FILES:
+            first_batch.append(full_file)
+        elif ext in RAW_FILES:
+            second_batch.append(full_file)
+
+    for file in first_batch + second_batch:
+        base_command = ['%(command)s %(file)s ' % {
+            'command': BASE_COMMAND,
+            'file': file
+        }]
+        if skip:
+            base_command.append("--skip '%s'" % skip)
+        if host:
+            base_command.append("--host %s" % host)
+        if tags:
+            base_command.append("--tags '%s'" % tags)
+        lines.append(' '.join(base_command))
+
+    with open(output, 'w') as fh:
+        fh.write('\n'.join(lines))
+
--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
             'start_api=photolog.api.main:start',
             'start_queue=photolog.queue.main:start',
             'start_web=photolog.web.main:start',
-            'upload2photolog=photolog.tools.uploader:run'
+            'upload2photolog=photolog.tools.uploader:run',
+            'prep_folder=photolog.tools.prep_folder:run'
         ]
     }
 )
