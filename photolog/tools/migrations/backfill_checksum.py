45fa20ab48bb90efe4dfc161ea5352f29e6285fa
Jj <jjdelc@gmail.com>
2015-12-30 20:21:16 -0500

    Don't need full settings, and perform db update

--- photolog/tools/migrations/backfill_checksum.py
+++ photolog/tools/migrations/backfill_checksum.py
@@ -9,19 +9,18 @@ other environments.
 import os
 from os.path import join
 from photolog.db import DB
-from photolog import settings_file
-from photolog.settings import Settings
 from photolog.services.base import file_checksum
 
 
 BASE_PATH = os.environ['BASE_PATH']
+DB_FILE = os.environ['DB_FILE']
 
 
-def migrate(db, settings):
-    with db._get_conn() as conn:
-        conn.execute('ALTER TABLE pictures ADD COLUMN checksum TEXT')
+def migrate(conn):
+    conn.execute('ALTER TABLE pictures ADD COLUMN checksum TEXT')
 
     total_ok, total_bad = 0, 0
+    updates = []
     for picture in conn.execute('SELECT * FROM pictures'):
         local_path = join(BASE_PATH, picture['year'],
             '%s%s' % (picture['month'], picture['day']))
@@ -41,13 +40,18 @@ def migrate(db, settings):
         if found:
             total_ok += 1
             print("Checksum for: %s: %s" % (found_file, checksum))
+            updates.append((picture['id'], checksum))
         else:
             print("File not found: %s" % local_path)
             total_bad += 1
-
     print('Total OK: %s - Bad: %s' % (total_ok, total_bad))
 
+    for pic_id, checksum in updates:
+        conn.execute('UPDATE pictures SET checksum =? WHERE id=?',
+            [pic_id, checksum])
+
+
 if __name__ == '__main__':
-    settings = Settings.load(settings_file)
-    db = DB(settings.DB_FILE)
-    migrate(db, settings)
+    db = DB(DB_FILE)
+    with db._get_conn() as conn:
+        migrate(conn)
