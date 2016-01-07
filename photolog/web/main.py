08d9dd0ec04604d33526ed43d28c55c1257b94d3
Jj <jjdelc@gmail.com>
2016-01-07 16:45:12 -0500

    nav agai

--- photolog/web/main.py
+++ photolog/web/main.py
@@ -133,7 +133,6 @@ def photo_list():
 
 
 def get_pic_nav(taken_time):
-    return {}
     prev_key, next_key = db.pictures.nav(taken_time)
     return {
         'prev': url_for('picture_detail', key=prev_key),
